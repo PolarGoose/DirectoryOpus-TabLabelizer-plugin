23c9b780363c8b18a1924ec1122c2e1960f2f58c
PolarGoose <35307286+PolarGoose@users.noreply.github.com>
2025-02-12 13:08:41 +0100

    Add "maximum tab length" configuration option. Use `~` symbol instead of `...` in tab labels.

--- build.ps1
+++ build.ps1
@@ -1,52 +1,47 @@
 Function Info($msg) {
-    Write-Host -ForegroundColor DarkGreen "`nINFO: $msg`n"
+  Write-Host -ForegroundColor DarkGreen "`nINFO: $msg`n"
 }
 
 Function Error($msg) {
-    Write-Host `n`n
-    Write-Error $msg
-    exit 1
+  Write-Host `n`n
+  Write-Error $msg
+  exit 1
 }
 
 Function CheckReturnCodeOfPreviousCommand($msg) {
-    if(-Not $?) {
-        Error "${msg}. Error code: $LastExitCode"
-    }
-}
-
-Function CreateZipArchive($file, $archiveFullName) {
-    Info "Create zip archive: `n '$archiveFullName' from file `n '$file'"
-    Compress-Archive -Force -Path $file -DestinationPath $archiveFullName
+  if(-Not $?) {
+    Error "${msg}. Error code: $LastExitCode"
+  }
 }
 
 Function CopyFile($file, $dstFolder) {
-    Info "Copy `n '$file' to `n '$dstFolder'"
-    New-Item -Force -ItemType "directory" $dstFolder > $null
-    Copy-Item -Force -Path $file -Destination $dstFolder > $null
+  Info "Copy `n '$file' to `n '$dstFolder'"
+  New-Item -Force -ItemType "directory" $dstFolder > $null
+  Copy-Item -Force -Path $file -Destination $dstFolder > $null
 }
 
 Function InsertVersionInsideScript($sciptFile, $version) {
-    Info "Insert version '$version' inside '$sciptFile'"
+  Info "Insert version '$version' inside '$sciptFile'"
 
-    $content = [System.IO.File]::ReadAllText($sciptFile).Replace( `
-        "    data.version = `"0.0-dev`";", `
-        "    data.version = `"$version`";")
-    [System.IO.File]::WriteAllText($sciptFile, $content)
+  $content = [System.IO.File]::ReadAllText($sciptFile).Replace( `
+    "  data.version = `"0.0-dev`"", `
+    "  data.version = `"$version`"")
+  [System.IO.File]::WriteAllText($sciptFile, $content)
 }
 
 Function GetVersion() {
-    $gitCommand = Get-Command -ErrorAction Stop -Name git
+  $gitCommand = Get-Command -ErrorAction Stop -Name git
 
-    $tag = & $gitCommand describe --exact-match --tags HEAD
-    if(-Not $?) {
-        $tag = "v0.0-dev"
-        Info "The commit is not tagged. Use '$tag' as a version instead"
-    }
+  $tag = & $gitCommand describe --exact-match --tags HEAD
+  if(-Not $?) {
+    $tag = "v0.0-dev"
+    Info "The commit is not tagged. Use '$tag' as a version instead"
+  }
 
-    $commitHash = & $gitCommand rev-parse --short HEAD
-    CheckReturnCodeOfPreviousCommand "Failed to get git commit hash"
+  $commitHash = & $gitCommand rev-parse --short HEAD
+  CheckReturnCodeOfPreviousCommand "Failed to get git commit hash"
 
-    return "$($tag.Substring(1))~$commitHash"
+  return "$($tag.Substring(1))~$commitHash"
 }
 
 Set-StrictMode -Version Latest
@@ -57,4 +52,3 @@ $version = GetVersion
 
 CopyFile $root/src/tab-labelizer.js $root/Build
 InsertVersionInsideScript $root/Build/tab-labelizer.js $version
-CreateZipArchive $root/Build/tab-labelizer.js $root/Build/DirectoryOpus-TabLabelizer-plugin
--- src/tab-labelizer.js
+++ src/tab-labelizer.js
@@ -1,140 +1,150 @@
-var folderAliasesToIgnore =  ["homeroot", "default", "last"]
+var folderAliasesToIgnore = ["homeroot", "default", "last"]
 
 function OnInit(data) {
-    data.name = "Tab labelizer";
-    data.desc = "Gives more detailed names to tabs";
-    data.version = "0.0-dev";
-    data.url = "https://github.com/PolarGoose/DirectoryOpus-TabLabelizer-plugin";
-    data.default_enable = true;
-    data.config_desc = DOpus.NewMap();
-    data.config.debug = false;
-    data.config_desc("debug") = "Print debug messages to the script log";
+  data.name = "Tab labelizer"
+  data.desc = "Gives more detailed names to tabs"
+  data.version = "0.0-dev"
+  data.url = "https://github.com/PolarGoose/DirectoryOpus-TabLabelizer-plugin"
+  data.default_enable = true
+  data.config_desc = DOpus.NewMap()
+  data.config.maxTabLabelLength = 40
+  data.config_desc("maxTabLabelLength") = "Specifies the maximum length of the tab label"
+  data.config.debug = false
+  data.config_desc("debug") = "Print debug messages to the script log"
 }
 
-function OnAfterFolderChange(data) {
-    debug("OnAfterFolderChange");
-    if (!data.result) {
-        return;
-    }
-    setTabName(data.tab);
+function OnAfterFolderChange(/* AfterFolderChangeData */ data) {
+  debug("OnAfterFolderChange")
+  if (!data.result) {
+    return
+  }
+  setTabName(data.tab)
 }
 
-function setTabName(tab) {
-    var displayPath = DOpus.fsutil().DisplayName(tab.path, "r");
-    debug("updateLister. originalPath='" + tab.path + "'; displayPath='" + displayPath + "'");
-    runDopusCommand('GO TABNAME="' + getTabLabel(displayPath) + '"', tab);
+function setTabName(/* Tab */ tab) {
+  var tabDisplayName = DOpus.fsutil().DisplayName(tab.path, "r")
+  debug("updateLister. originalPath='" + tab.path + "'; tabDisplayName='" + tabDisplayName + "'")
+  runDopusCommand('GO TABNAME="' + getTabLabel(tabDisplayName) + '"', tab)
 }
 
-function getTabLabel(displayPath) {
-    if (startsWith(displayPath, "ftp://")) {
-        // ftp paths are not supported by this script, don't alter the existing DOpus behavior
-        return "";
-    }
-
-    displayPath = tryApplyAlias(displayPath)
-    return getTruncatedLabel(displayPath);
+function getTabLabel(path) {
+  // ftp paths are not supported by this script, don't alter the existing DOpus behavior
+  if (startsWith(path, "ftp://")) {
+    return ""
+  }
+  return getLabel(tryApplyAlias(path))
 }
 
-// If there is an alias defined in "Folder Aliases", returns the path starting from this alias name, otherwise returns the same unmodified path.
-// Example: "C:\Users\Public\Folder1\Folder2\Folder3" => "commonfiles\Folder1\Folder2\Folder3"
+// If there is an alias defined in "Folder Aliases", returns the path starting from the alias name.
+// Example:
+//   "C:\Users\Public\Folder1\Folder2\Folder3" => "commonfiles\Folder1\Folder2\Folder3"
 function tryApplyAlias(path) {
-    var alias = getAlias(path)
-    if(alias === null) {
-        return path;
-    }
+  var alias = getBestMatchingAlias(path)
 
-    var aliasPath = String(alias.Path)
-    if(lastSymbol(aliasPath) === "\\") {
-        return alias + path.substring(aliasPath.length - 1);
-    }
-    return alias + path.substring(aliasPath.length);
+  // if no alias is defined, return the original path
+  if (alias === null) {
+    return path
+  }
+
+  var aliasPath = String(alias.Path)
+  // If path ends with a backslash, adjust substring index accordingly
+  var suffixStart = (lastSymbol(aliasPath) === "\\") ? (aliasPath.length - 1) : aliasPath.length
+
+  return alias + path.substring(suffixStart)
 }
 
-function getAlias(path) {
-    // Before accessing Aliases dictionary, we need to update it in case a user added new aliases while the script was running
-    DOpus.Aliases.Update()
+// Returns the alias with the longest matching prefix for a given path, ignoring any from “folderAliasesToIgnore”.
+// There can be different aliases that are suitable for a given path. We choose the one with the longest path, because it is the most specific.
+// For example the path "C:\Users\userName\AppData\Roaming\GPSoftware", can be converted to "profile\AppData\Roaming\GPSoftware" or "appdata\GPSoftware".
+// We need to take the second one.
+function /* Alias */ getBestMatchingAlias(path) {
+  DOpus.Aliases.Update()
 
-    matchingAliases = []
-    for (var e = new Enumerator(DOpus.Aliases); !e.atEnd(); e.moveNext()) {
-        var alias = e.item();
+  var matchingAliases = []
+  for (var e = new Enumerator(DOpus.Aliases); !e.atEnd(); e.moveNext()) {
+    var alias = e.item()
 
-        if(contains(folderAliasesToIgnore, alias)) {
-            continue;
-        }
-
-        if(startsWith(path, alias.Path)) {
-            matchingAliases.push(alias);
-        }
+    // Skip ignored aliases
+    if(contains(folderAliasesToIgnore, alias)) {
+      continue;
     }
 
-    if(matchingAliases.length === 0) {
-        return null
+    if (startsWith(path, alias.Path)) {
+      matchingAliases.push(alias)
     }
+  }
 
-    return getAliasWithLongestPath(matchingAliases)
+  if (matchingAliases.length === 0) {
+    return null
+  }
+
+  // Return the alias with the longest Path
+  return matchingAliases.sort(function(a, b) { return b.Path.length - a.Path.length })[0]
 }
 
-function getAliasWithLongestPath(aliasesArray) {
-    return aliasesArray.sort(function (a, b) { return new String(b.Path).length - new String(a.Path).length })[0];
-}
+// Converts path into a shorter form. Examples:
+//   C:\host\share\subdir1\subdir2\subdir3 => C:\host\~\subdir2\subdir3
+//   \\host\share\subdir1\subdir2\subdir3 => \\host\share\~\subdir2\subdir3
+function getLabel(path) {
+  var pathParts = splitWithoutEmptyElements(path, "\\")
 
-function getTruncatedLabel(path) {
-    var pathParts = splitWithoutEmptyElements(path, '\\');
-    if (pathParts.length <= 4) {
-        return truncateIfTooLong(path)
-    }
+  if (pathParts.length <= 4) {
+    return truncateIfTooLong(path)
+  }
 
-    var uncPathPrefix = startsWith(path, "\\\\") ? "\\\\" : "";
-    var res = uncPathPrefix + pathParts[0] + "\\" + pathParts[1] + "\\..\\" + pathParts[pathParts.length - 2] + "\\" + pathParts[pathParts.length - 1];
-    return truncateIfTooLong(res)
+  var uncPrefix = startsWith(path, "\\\\") ? "\\\\" : ""
+  var shortenedPath = uncPrefix + pathParts[0] + "\\" + pathParts[1] + "\\~\\" + pathParts[pathParts.length - 2] + "\\" + pathParts[pathParts.length - 1]
+  return truncateIfTooLong(shortenedPath)
 }
 
 function splitWithoutEmptyElements(str, separator) {
-    return removeEmptyElements(str.split(separator));
+  return removeEmptyElements(str.split(separator))
 }
 
 function truncateIfTooLong(label) {
-    return label.length <= 40 ? label : label.substring(0, 40) + "~"
+  if (label.length <= Script.config.maxTabLabelLength) {
+    return label
+  }
+  return label.substring(0, Script.config.maxTabLabelLength) + "~"
 }
 
 function contains(array, obj) {
-    var i = array.length;
-    while (i--) {
-       if (array[i] == obj) {
-           return true;
-       }
+  var i = array.length;
+  while (i--) {
+    if (array[i] == obj) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function startsWith(str, prefix) {
-    return str.indexOf(prefix) === 0
+  return str.indexOf(prefix) === 0
 }
 
 function lastSymbol(str) {
-    return str[str.length - 1]
+  return str.charAt(str.length - 1)
 }
 
 function removeEmptyElements(stringArray) {
     var res = []
     for (i = 0; i < stringArray.length; i++) {
         if (stringArray[i].length) {
-            res.push(stringArray[i]);
+            res.push(stringArray[i])
         }
     }
-    return res;
+    return res
 }
 
 function runDopusCommand(command, tab) {
-    debug("runDopusCommand '" + command + "'");
-
-    var dopusCommand = DOpus.NewCommand;
-    dopusCommand.SetSourceTab(tab);
-    dopusCommand.RunCommand(command);
+  debug("runDopusCommand '" + command + "'")
+  var cmd = DOpus.NewCommand()
+  cmd.SetSourceTab(tab)
+  cmd.RunCommand(command)
 }
 
 function debug(text) {
-    if (Script.config.debug) {
-        DOpus.Output(text);
-    }
+  if (Script.config.debug) {
+    DOpus.Output(text)
+  }
 }
